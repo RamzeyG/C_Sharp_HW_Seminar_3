bc1933018ee88ef2390228bf18d026eca7c505d5
Ramzey <remzesii@gmail.com>
2022-05-22 11:50:16 +0500

    Сделано третья задание

---
+++ ex003/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void func(int b){
+    int count = 1;
+    while(count <= b){
+        int x = count*count*count;
+        Console.WriteLine($"{count}^3 = {x}");
+        count ++;
+    }
+}
+
+Console.WriteLine("Введите любое целое число");
+
+string a = Console.ReadLine();
+int b = int.Parse(a);
+
+
+func(b);
+
+
+
+//Console.WriteLine("Hello, World!");
